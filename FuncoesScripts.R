2967911b455d2dc927e2f4b55f7e07f6045b784d
ThiagoRBM <52140942+ThiagoRBM@users.noreply.github.com>
2022-04-17 11:23:52 -0300

    Add files via upload

--- FuncoesScripts.R
+++ FuncoesScripts.R
@@ -253,3 +253,58 @@ areaFoliar= function(CaminhoImg, DPI){
   DPIparaCM2= (DPI/2.54)^2
   areaFoliar= computeFeatures.shape(Img)[1]/DPIparaCM2
 }
+
+removerSujeira = function(ImagemNumerada, VetorNumeros) {
+  mt = matrix(nrow = nrow(ImagemNumerada),
+              ncol = ncol(ImagemNumerada))
+  for(linha in 1:ncol(ImagemNumerada)){
+    vals= ImagemNumerada[,linha]
+    if(sum(vals) > 0){
+      for(x in 1:length(vals)){
+        if(vals[x] %in% names(VetorNumeros)){
+          mt[x,linha]= vals[x]
+        } 
+        else{mt[x,linha] = 0}
+      } 
+      
+    } else{mt[x,length(vals)]=0}
+  }
+  print("imagem limpa")
+  return(mt)
+}
+
+extrairContorno= function(imagemLimpa){
+  cont= ocontour(imagemLimpa)
+  
+  LISTA= list()
+  for(i in 1:length(cont)){ ## transformando o objeto com os contornos (cont) em um data frame
+    lis= cont[[i]] %>% 
+      as.data.frame() %>% 
+      mutate(obj= as.numeric(names(cont)[i])) %>% 
+      rename(y= V1,
+             x=V2)
+    
+    LISTA[[i]]= lis
+    if(i == length(cont)){
+      LISTA= do.call("rbind",LISTA)
+    }
+    
+  }
+  
+  mt= matrix(nrow = nrow(imagemLimpa),
+             ncol = ncol(imagemLimpa))
+  for(i in 1:ncol(mt)){## pegando o contorno gerado na funcao ocontour e juntando como estava na imagem inicial
+    df=LISTA[LISTA$x==i,]
+    if(nrow(df)>0){
+      for(x in 1:nrow(df)){
+        y=df$y[x]
+        mt[y,i]=df$obj[1]
+      }
+    }else{mt[c(1:nrow(mt)),i]=0}
+    
+    
+  }
+  print("controno extraido")
+  return(mt)
+  
+}
